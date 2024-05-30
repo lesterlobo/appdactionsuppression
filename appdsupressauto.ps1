e53fc4327c4d45c06b2f447c64de91a5654f827b
lesterlobo <lester.lobo@gmail.com>
2024-05-30 12:52:12 -0500

    Update appdsupressauto.ps1
    Added Logging Functionality and updated timezone to convert local timezone to gmt.

--- appdsupressauto.ps1
+++ appdsupressauto.ps1
@@ -1,13 +1,27 @@
-  ########################################################################################
+########################################################################################
 #### Powershell Script to suppress AppDynamics machine agent alerts for current server
 ########################################################################################
-
 
 # Initiatize the connection parameters
 $controller = ""
 $serverId = ""
 $USERNAME = ""
 $PASSWORD = ""
+
+$Logdir = "c:\zco\logs\"
+If (!(Test-Path -Path $Logdir)){New-Item $Logdir -ItemType directory}
+$LogfileName = "AppdSuppression"
+$Logfile = $Logdir+$LogfileName+".log"
+ 
+Function Write-Log {
+   Param ([string]$logstring)
+   #replace any nulls with spaces
+    $logstring = [regex]::Replace($logstring,[char]0,' ')
+ 
+$timestamp = (Get-Date -format g)
+   Add-content $Logfile -value ($timestamp + ": " + $logstring)
+}
+
 
 
 # Check if AppDynamics Machine Agent is running on this server to trigger action suppression.
@@ -18,17 +32,17 @@ $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
 
 # Check if the service object exists and if it's running
 if ($service -ne $null -and $service.Status -eq "Running") {
-    Write-Host "The service '$serviceName' is running."
+    Write-Log "The service '$serviceName' is running."
     # Obtain Machine Agent Install Path
     # Get the service object
     $serviceObject = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
 
 
-    Write-Host "Service Path: $($serviceObject.PathName)"
+    Write-Log "Service Path: $($serviceObject.PathName)"
 
     # Use Split-Path to separate the path into its components
     $FilePath = (Split-Path -Path $($serviceObject.PathName)) + "\MachineAgentService.vmoptions"
-    Write-Host "Service Path: $FilePath"
+    Write-Log "Service Path: $FilePath"
 
     #$results = Select-String -Path $FilePath -Pattern "appdynamics.controller.hostName" -SimpleMatch
     $results = Get-Content $FilePath | Where-Object {$_ -like "*appdynamics.controller.hostName*"}
@@ -36,12 +50,12 @@ if ($service -ne $null -and $service.Status -eq "Running") {
     if ($results -ne $null) {
 
         $controller = $results.split("=")[1]
-        Write-Host "Controller Host: $controller"
+        Write-Log "Controller Host: $controller"
 
         $results = Get-Content $FilePath | Where-Object {$_ -like "*appdynamics.agent.accountName*"}
         $accountName = $results.split("=")[1]
         $USERNAME = $USERNAME + "@" + $accountName
-        Write-Host "Controller Account: $accountName"
+        Write-Log "Controller Account: $accountName"
 
         $proxy = $false
 
@@ -49,12 +63,12 @@ if ($service -ne $null -and $service.Status -eq "Running") {
         if ($results -ne $null) {
             $proxyHost = $results.split("=")[1]
             $proxy = $true
-            Write-Host "Controller Proxy Host: $proxyHost"
+            Write-Log "Controller Proxy Host: $proxyHost"
         }
         $results = Get-Content $FilePath | Where-Object {$_ -like "*appdynamics.http.proxyPort*"}
         if ($results -ne $null) {
             $proxyPort = $results.split("=")[1]
-            Write-Host "Controller Proxy Port: $proxyPort"
+            Write-Log "Controller Proxy Port: $proxyPort"
         }
 
         # Retrieve Server Application Id
@@ -81,7 +95,7 @@ if ($service -ne $null -and $service.Status -eq "Running") {
         $response.Content   
         
         $serverId = ([xml]$response.Content).applications.application.id
-        Write-Host "Server ID: $serverId"    
+        Write-Log "Server ID: $serverId"    
         
         # Get the current date and time
          # Get the local timezone
@@ -89,14 +103,16 @@ if ($service -ne $null -and $service.Status -eq "Running") {
         $timezone = $($localTimeZone.Id) 
 
         $currentDateTime = (Get-Date)
+        $gmtDateTime = $currentDateTime.ToUniversalTime()
+        $gmtDateTime
         
         # Format the date
-        $dateString = $currentDateTime.ToString("yyyy-MM-ddTHH:mm:ss")
+        $dateString = $gmtDateTime.ToString("yyyy-MM-ddTHH:mm:ss")
         
         $uri = "https://"+ $controller + "/controller/alerting/rest/v1/applications/"+  $serverId + "/action-suppressions"
         $name = "ChangeSuppression:" + $env:computername
         $startTime = $dateString
-        $endTime = $currentDateTime.AddMinutes(240).ToString("yyyy-MM-ddTHH:mm:ss")
+        $endTime = $gmtDateTime.AddMinutes(240).ToString("yyyy-MM-ddTHH:mm:ss")
         $serverName = hostname
         
         $serverName
@@ -104,7 +120,6 @@ if ($service -ne $null -and $service.Status -eq "Running") {
         $jsonBody = @{
             "name" = $name
             "disableAgentReporting" = "true"
-            "timezone" = "$timezone"
             "startTime" = "$startTime"
             "endTime" = "$endTime"
             "affects" = @{
@@ -149,21 +164,14 @@ if ($service -ne $null -and $service.Status -eq "Running") {
         # Output the response
         $response 
  
-
-        
-        
-    
     } else {
-        Write-Host "Service Path: $FilePath"
+        Write-Log "Service Path: $FilePath"
     }
 
-    
-
-
 } elseif ($service -ne $null -and $service.Status -ne "Running") {
-    Write-Host "The service '$serviceName' is not running."
+    Write-Log "The service '$serviceName' is not running."
 } else {
-    Write-Host "The service '$serviceName' does not exist."
+    Write-Log "The service '$serviceName' does not exist."
 }  
 
 
@@ -173,3 +181,5 @@ if ($service -ne $null -and $service.Status -eq "Running") {
   
  
  
+ 
+
